1d5af44b639bfaba194c6fab86714a490f1a9f21
up201506197 <up201506197@fe.up.pt>
2017-11-27 14:28:23 +0000

    update

---
+++ templates/category/list_categories.php
@@ -0,0 +1,12 @@
+<section id="categories">
+  <h2>Categories</h2>
+  <ul>
+    <li><a href="?cat_id=1">Finances</a></li>
+    <li><a href="?cat_id=2">Books</a></li>
+    <li><a href="?cat_id=3">Sports</a></li>
+    <li><a href="?cat_id=4">Shopping</a></li>
+    <li><a href="?cat_id=5">Home Care</a></li>
+    <li><a href="?cat_id=6">Family</a></li>
+    <li><a href="?cat_id=7">Friends</a></li>
+  </ul>
+</section>
