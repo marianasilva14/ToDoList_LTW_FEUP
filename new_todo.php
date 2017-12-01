0328cd185969644fb515744d0ddf0c3d0eddbe4c
rjstorres <r_torres96@live.com.pt>
2017-12-01 19:32:46 +0000

    update still wtf

--- new_todo.php
+++ new_todo.php
@@ -14,15 +14,26 @@ $stmt->execute(array($category));
 $result=$stmt->fetch();
 
 
-if(!result)
+if(!$result)
     header("Location: logged.php");
 
 
 
-$stmt =$dbh->prepare('INSERT INTO to_do(toDO_id,toDO_description,cat_id,usr_id)
-VALUES (?,?,?,?)');
-$stmt->execute(array(10,$description,$result['cat_id'], $_SESSION['usr_info']['usr_id']);
 
+$stmt2 =$dbh->prepare("INSERT INTO to_do(toDO_id,toDO_description,cat_id,usr_id)
+VALUES (?,?,?,?)");
+$stmt2->execute(array(NULL,$description,$result['cat_id'], $_SESSION['usr_info']['usr_id']));
+
+
+/*$stmt3 = $dbh->prepare("SELECT * FROM to_do");
+$stmt3->execute();
+
+foreach ($stmt3->fetch() as $result2){
+    echo $result2['toDO_description'];
+    echo $result2['usr_id'];
+}*/
+
+header("Location: logged.php");
 
 
 
