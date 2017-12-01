43e3d218c58b206ed082d1049016c0e6478e557e
rjstorres <r_torres96@live.com.pt>
2017-12-01 17:45:27 +0000

    what

--- templates/category/list_categories.php
+++ templates/category/list_categories.php
@@ -10,7 +10,31 @@
 </section>
 
 <header id="buttons">
-  <a class="addTask"><b>Add Task</b></a>
+  
+
+  <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+  <script type="text/javascript" src="../../scripts/register.js"></script>
+  <a class="NewTask" data-popup-open="popup-1" href="#"><b>Add Task</b></a>
+    <div class="popup-register" data-popup-register="popup-1">
+      <div class="popup-inner-register">
+
+        <form action="new_user.php" method="post"  enctype="multipart/form-data">
+          <input class="inputField-register" type="text" id="name" required="required" name="name" placeholder="Name">
+          <input class="inputField-register" type="text" id="username" required="required" name="username" placeholder="Username">
+          <input class="inputField-register" type="text" id="age" required="required" name="age" placeholder="Age">
+          <input class="inputField-register" type="email" id="email" required="required" name="email" placeholder="Email">
+          <input class="inputField-register" type="password" minlength="8" id="password" name="password" required="required" placeholder="Password">
+          <input class="inputField-registerPhoto" type="file" id="photo" name="photo" required="required" placeholder="Photo">
+          <br></br>
+          <input id="submit" type="submit" value ="Register">
+        </form>
+
+        <a class="popup-close-register" data-popup-register-close="popup-1" href="#"></a>
+      </div>
+    </div>
+
+
+
   <a class="edit_profile" data-popup-open="popup-1" href="#"><b>Edit your profile</b></a>
 
     <div class="popup" data-popup="popup-1">
