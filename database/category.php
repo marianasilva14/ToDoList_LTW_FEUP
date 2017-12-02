9ea4ae0b960a4a283e217f9fb0e68611e724a9f3
Francisca Cerquinho <francisca@MBPdeFrancisca.lan>
2017-12-02 12:14:45 +0000

    add task done

--- database/category.php
+++ database/category.php
@@ -8,8 +8,24 @@
 
   function getAllToDoLists($cat_id) {
     global $dbh;
-    $stmt = $dbh->prepare("SELECT cat_name, toDO_description FROM to_do JOIN category ON(category.cat_id = to_do.cat_id) WHERE toDO_id=?");
+    $stmt = $dbh->prepare("SELECT cat_name, toDO_description FROM to_do JOIN category ON(category.cat_id = to_do.cat_id)
+    WHERE category.cat_id=?");
     $stmt->execute(array($cat_id));
     return $stmt->fetchAll();
   }
+
+function insert_new_toDoList($category,$description){
+  global $dbh;
+  $stmt = $dbh->prepare("SELECT * FROM category WHERE cat_name=?");
+  $stmt->execute(array($category));
+  $result=$stmt->fetch();
+
+  if(!$result)
+      header("Location: logged.php");
+
+  $stmt2 =$dbh->prepare("INSERT INTO to_do(toDO_id,toDO_description,cat_id,usr_id)
+  VALUES (?,?,?,?)");
+  $stmt2->execute(array(NULL,$description,$result['cat_id'], $_SESSION['usr_info']['usr_id']));
+}
+
 ?>
--- new_todo.php
+++ new_todo.php
@@ -3,48 +3,16 @@
 
 session_start();
 include_once('includes/init.php');
+include_once('database/category.php');
 
 // Ler dados vindos do post
 $description = $_POST['Description'];
 $category = $_POST['Category'];
 
-/*
-//verificar se a categoria existe
-$consulta = mysql_query("SELECT * FROM category WHERE cat_name = '$category'");
-$numRegistros = mysql_num_rows($consulta);
-if( $numRegistros == 0){
-
-}else{
-
-}
-*/
-global $dbh;
-$stmt = $dbh->prepare("SELECT * FROM category WHERE cat_name=?");
-$stmt->execute(array($category));
-$result=$stmt->fetch();
-
-
-if(!$result)
-    header("Location: logged.php");
-
-
-
-
-$stmt2 =$dbh->prepare("INSERT INTO to_do(toDO_id,toDO_description,cat_id,usr_id)
-VALUES (?,?,?,?)");
-$stmt2->execute(array(NULL,$description,$result['cat_id'], $_SESSION['usr_info']['usr_id']));
-
-
-/*$stmt3 = $dbh->prepare("SELECT * FROM to_do");
-$stmt3->execute();
-
-foreach ($stmt3->fetch() as $result2){
-    echo $result2['toDO_description'];
-    echo $result2['usr_id'];
-}*/
+insert_new_toDoList($category,$description);
 
 header("Location: logged.php");
 
-  ob_end_flush();
+ob_end_flush();
 
 ?>
