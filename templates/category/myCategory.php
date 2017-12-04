4ac6c080d9282b7fb65bdae47b91ebba60d54172
rjstorres <r_torres96@live.com.pt>
2017-12-04 22:30:50 +0000

    pop-up mark as completed not working

--- templates/category/myCategory.php
+++ templates/category/myCategory.php
@@ -18,8 +18,9 @@
      <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
      <script type="text/javascript" src="scripts/list_categories.js"></script>
      <script type="text/javascript" src="scripts/add-to-do.js"></script>
+     <script type="text/javascript" src="scripts/mark-as-completed.js"></script>
      <script type="text/javascript" src="scripts/delete-to-do.js"></script>
-    <script type="text/javascript" src="scripts/calendar.js"></script>
+     <script type="text/javascript" src="scripts/calendar.js"></script>
    <?php } ?>
   </ul>
 
@@ -27,6 +28,24 @@
 
 
 <aside id="buttons_MyCategory">
+
+<a class="markTask" data-popup-deleteTask-open="popup-1" href="#"><b>Mark Task as completed</b></a>
+  <div class="popup-markTask" data-popup-deleteTask="popup-1">
+    <div class="popup-inner-markTask">
+
+      <form action="mark_as_completed.php" method="post">
+         <select class="dropdown"name="Category" >
+            <li><option class="cat" id="Category" name="Category" href="#"><?=$categories[0]['cat_name']?></option></li>
+        </select>
+        <input class="inputField-markTask" type="text" id="Description" required="required" name="Description" placeholder="Description">
+        <br></br>
+        <input id="submit" type="submit" value ="markTask">
+      </form>
+
+      <a class="popup-close-markTask" data-popup-close-deleteTask="popup-1" href="#"></a>
+    </div>
+  </div>
+
   <a class="delete_task" data-popup-deleteTask-open="popup-1" href="#"><b>Delete task</b></a>
   <div class="popup-deleteTask" data-popup-deleteTask="popup-1">
     <div class="popup-inner-deleteTask">
@@ -43,6 +62,7 @@
       <a class="popup-close-deleteTask" data-popup-close-deleteTask="popup-1" href="#"></a>
     </div>
   </div>
+
   <a class="NewTask" data-popup-addTask-open="popup-1" href="#"><b>Add Task</b></a>
   <div class="popup-addTask" data-popup-addTask="popup-1">
     <div class="popup-inner-addTask">
