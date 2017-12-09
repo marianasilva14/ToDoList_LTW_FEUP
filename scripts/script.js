396e4adef3ca679681e680f9ae9878db6dded157
rjstorres <r_torres96@live.com.pt>
2017-12-09 17:02:00 +0000

    minor change to script

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,43 @@
-list= document.getElementsByClassName("wrapper");
+list = document.getElementsByClassName("wrapper");
 
 if (list != null) {
-    
+
     let forms = document.getElementsByTagName('form');
-    
+
     forms[2].addEventListener('submit', function (event) {
         let id = forms[2].children[0].value;
-        let index = 0;
         id = parseInt(id, 10);
 
-        for (index; index < list.length; index++) {
-            if (id == parseInt(list[index].innerText, 10)) {
-                list[index].parentNode.removeChild(list[index]);
-                break;
+        let xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("POST", "delete_to_do_list.php", true);
+        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xmlhttp.onreadystatechange = function () {
+            if (this.readyState === 4 || this.status === 200) {
+                updateDropdownsAndScreen_Todolists(id); // echo from php
             }
-        }
+        };
+        xmlhttp.send("to_do_listID=" + id);
 
-        var selectobject=document.getElementById("todolistToEliminate");
-        for (var i=0; i<selectobject.length; i++){
-            if (parseInt(selectobject.options[i].value) == id) {
-                selectobject.remove(i);
-                break;
-            } 
-        }
-
-
-        if (index < list.length+1) {
-            console.log("LEGIT?")
-            let xmlhttp = new XMLHttpRequest();
-            xmlhttp.open("POST", "delete_to_do_list.php", true);
-            xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xmlhttp.onreadystatechange = function () {
-                if (this.readyState === 4 || this.status === 200) {
-                    console.log(this.responseText); // echo from php
-                }
-            };
-            xmlhttp.send("to_do_listID=" + id);
-        }
 
         event.preventDefault();
     });
 
 }
 
+function updateDropdownsAndScreen_Todolists(id) {
+    let index = 0;
+    for (index; index < list.length; index++) {
+        if (id == parseInt(list[index].innerText, 10)) {
+            list[index].parentNode.removeChild(list[index]);
+            break;
+        }
+    }
+
+    var selectobject = document.getElementById("todolistToEliminate");
+    for (var i = 0; i < selectobject.length; i++) {
+        if (parseInt(selectobject.options[i].value) == id) {
+            selectobject.remove(i);
+            break;
+        }
+    }
+}
