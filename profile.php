2dab89bc1f0be4e358b601f02edb00b2ab708988
FranciscaCerquinho <franciscacerquinho@gmail.com>
2017-12-12 15:33:36 +0000

    almost final version

--- profile.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once('includes/session_start.php');
-include_once('database/user.php');
-
-include_once('templates/common/header.php');
-include_once('templates/category/list_categories.php');
-include_once('templates/common/footer.php');
-?>
